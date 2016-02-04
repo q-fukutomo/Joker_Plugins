ee972b5e0362c273108b78f9d9d1d6fa6e48bdab
q-fukutomo <katatsuki52@gmail.com>
2016-02-05 00:39:26 +0900

    to variable shake-width, shake-height and shake-time

--- QuakePluginComponent.cs
+++ QuakePluginComponent.cs
@@ -5,12 +5,21 @@ using System.Collections.Generic;
 
 namespace Novel{
     public class QuakeComponent:AbstractComponent{
-        public QuakeComponent(){}
-
+        public QuakeComponent(){
+            this.originalParam = new Dictionary<string,string>{
+                {"x","1.5"},
+                {"y","1.5"},
+                {"time","5.0"}
+            };
+        }
+        
         GameObject target = GameObject.Find("MainCamera");
-
+        
         public override void start(){
-            iTween.ShakePosition(target, iTween.Hash("x",1.5, "y",1.5, "time",5.0f));
+            double x = double.Parse(this.param["x"]);
+            double y = double.Parse(this.param["y"]);
+            float time = float.Parse(this.param["time"]);
+            iTween.ShakePosition(target, iTween.Hash("x",x, "y",y, "time",time));
             this.gameManager.nextOrder();
         }
     }
